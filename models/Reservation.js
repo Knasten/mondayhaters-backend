17c22fdf4a94f2ca3b58c6156be3c16ccc8c5097
Knasten <hampusjojo@gmail.com>
2023-11-05 00:16:15 +0100

    add reservation model

--- models/Reservation.js
+++ models/Reservation.js
@@ -1,21 +1,38 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './db/test.sqlite'
-})
-
-const RaidTag = sequelize.define('tag', {
-  name: {
-    type: Sequelize.DataTypes.STRING
-  },
-})
+const Sequelize = require('sequelize')
+const {sequelize} = require('./Connection')
 
 const Reservation = sequelize.define('reservation', {
-  userId: {
+  raid: {
+    type: Sequelize.DataTypes.STRING,
+    unique: true
+  },
+  100: {
     type: Sequelize.DataTypes.INTEGER
   },
-  raid: {
-    type: Sequelize.DataTypes.STRING
-  }
+  90: {
+    type: Sequelize.DataTypes.INTEGER
+  },
+  80: {
+    type: Sequelize.DataTypes.INTEGER
+  },
+  70: {
+    type: Sequelize.DataTypes.INTEGER
+  },
+  60: {
+    type: Sequelize.DataTypes.INTEGER
+  },
+  55: {
+    type: Sequelize.DataTypes.INTEGER
+  },
+  50: {
+    type: Sequelize.DataTypes.INTEGER
+  },
 })
+
+Reservation.sync({force:true}).then(() => {
+  console.log('RESERVATION: Success')
+}).catch(e => {
+  console.error('RESERVATION: Error when syncing')
+})
+
+module.exports = { Reservation, sequelize }
